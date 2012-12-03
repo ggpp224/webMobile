04616b73ded01117980a0634fb23965552aecd49
guopeng <guopeng@DPEK1CNG8290Y19.Ambow.com>
2012-12-03 15:46:05 +0800

    add data

---
+++ data/list.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: application/json;charset=utf-8');
+?>
+
+[
+	{"id":1,"name":"视频一","sex":"man","url":"http://10.10.6.78/h264/video1.mp4"},
+	{"id":2,"name":"视频二","sex":"wman","url":"http://10.10.6.78/h264/video2.mp4"},
+	{"id":3,"name":"视频三","sex":"man","url":"http://10.10.6.78/h264/video3.mp4"},
+	{"id":4,"name":"视频四","sex":"wman","url":"http://10.10.6.78/h264/video4.mp4"}
+]
