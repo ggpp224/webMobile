90e2d17202879a1daf4c9893b23f635c66a9fa37
Admin <Admin@10.10.27.39>
2012-12-04 17:26:07 +0800

    refresh

--- app/controller/test1.js
+++ app/controller/test1.js
@@ -4,14 +4,27 @@
  * @description index
  */
 
-App.controller = Ambow.extend(App.Controller,{
-	tplName:'test1',
-	
-	control: function(){
-		alert(11);		
-	}
-	
-	
+App.controller = Ambow.extend(App.Controller, {
+			tplName : 'test1',
 
-});
+			control : function() {
+				Ambow.getJSON('http://localhost/webMobile/data/list.php',
+						function(data) {
+							var htm = '';
+							Ambow.each(data, function(rec, idx) {
+										htm += '<li><a href="#">' + rec.name
+												+ '</a></li>';
+									});
+
+							$('#video_list').html(htm);
+							$('#video_list').listview("refresh");
+							
+							
+							$('#video_list').bind('click', function(e, ui) {
+										alert(1);
+							});
+						});
+			}
+
+		});
 new App.controller();
